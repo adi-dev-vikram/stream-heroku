c32828beca1b43997fffa0c55d78ec1d79e31198
Aditya Vikram <65796572+adi-dev-vikram@users.noreply.github.com>
2022-03-15 22:03:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,69 +1,177 @@
 import streamlit as st
 import pandas as pd
-import base64
-import matplotlib.pyplot as plt
 import seaborn as sns
+import csv
+import base64
+import pandas as pd
+from prettytable import PrettyTable
 import numpy as np
 
-st.title('NFL Football Stats (Rushing) Explorer')
+from google.colab import files
+from scapy.all import *
+import matplotlib.pyplot as plt
+from collections import Counter
+
+st.title('PCAP File Analyser')
 
 st.markdown("""
-This app performs simple webscraping of NFL Football player stats data (focusing on Rushing)!
-* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
-* **Data source:** [pro-football-reference.com](https://www.pro-football-reference.com/).
+This app performs simple pcap analysis and provides insights about traffic data along with downloadable reports which saves time !
 """)
 
-st.sidebar.header('User Input Features')
-selected_year = st.sidebar.selectbox('Year', list(reversed(range(1990,2020))))
+st.header('Display essentials stats derived from PCAP file')
+#st.write(pcap name)
+#st.dataframe(data)
 
-# Web scraping of NFL player stats
-# https://www.pro-football-reference.com/years/2019/rushing.htm
-@st.cache
-def load_data(year):
-    url = "https://www.pro-football-reference.com/years/" + str(year) + "/rushing.htm"
-    html = pd.read_html(url, header = 1)
-    df = html[0]
-    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
-    raw = raw.fillna(0)
-    playerstats = raw.drop(['Rk'], axis=1)
-    return playerstats
-playerstats = load_data(selected_year)
 
-# Sidebar - Team selection
-sorted_unique_team = sorted(playerstats.Tm.unique())
-selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
-
-# Sidebar - Position selection
-unique_pos = ['RB','QB','WR','FB','TE']
-selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
-
-# Filtering data
-df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
-
-st.header('Display Player Stats of Selected Team(s)')
-st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
-st.dataframe(df_selected_team)
-
-# Download NBA player stats data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-def filedownload(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+# Download stats
+def filedownload(csv_file):
+    b64 = base64.b64encode(csv_file.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="pcapstats.csv">Download CSV File</a>'
     return href
 
-st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+PIE_PLOT_DATA = []
 
-# Heatmap
-if st.button('Intercorrelation Heatmap'):
-    st.header('Intercorrelation Matrix Heatmap')
-    df_selected_team.to_csv('output.csv',index=False)
-    df = pd.read_csv('output.csv')
+def read_pcap(packets_list):
+  count_UDP = count_TCP = 0
+  
+  for pkt in packets_list:
+    if pkt.haslayer(DNSQR):
+      query = pkt[DNSQR].qname
+      print(query)
+    if pkt.haslayer(IP):
+      pckt_src=pkt[IP].src
+      pckt_dst=pkt[IP].dst
+      pckt_ttl=pkt[IP].ttl
+      print("IP Packet: {} is going to {} and has ttl value {}".format(pckt_src,pckt_dst,pckt_ttl))
+    if pkt.haslayer(IP):
+      if pkt[IP].proto == 17:
+        count_UDP += 1
+      if pkt[IP].proto ==6:
+        count_TCP += 1
 
-    corr = df.corr()
-    mask = np.zeros_like(corr)
-    mask[np.triu_indices_from(mask)] = True
-    with sns.axes_style("white"):
-        f, ax = plt.subplots(figsize=(7, 5))
-        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-    st.pyplot()
+  PIE_PLOT_DATA.append(count_UDP)
+  PIE_PLOT_DATA.append(count_TCP)
+  piePlot()
+
+def piePlot():
+  DATA_TYPE = ['UDP','TCP']
+  explode = (0.1, 0.0)
+ 
+  # Creating color parameters
+  colors = ( "indigo", "beige")
+ 
+  # Wedge properties
+  wp = { 'linewidth' : 1, 'edgecolor' : "black" }
+ 
+  # Creating autocpt arguments
+  def func(pct, allvalues):
+    absolute = int(pct / 100.*np.sum(allvalues))
+    return "{:.1f}%\n({:d} g)".format(pct, absolute)
+ 
+  # Creating plot
+  fig, ax = plt.subplots(figsize =(10, 7))
+  wedges, texts, autotexts = ax.pie(PIE_PLOT_DATA,
+                                  autopct = lambda pct: func(pct, PIE_PLOT_DATA),
+                                  explode = explode,
+                                  labels = DATA_TYPE,
+                                  shadow = True,
+                                  colors = colors,
+                                  startangle = 90,
+                                  wedgeprops = wp,
+                                  textprops = dict(color ="magenta"))
+ 
+  # Adding legend
+  ax.legend(wedges, DATA_TYPE,
+          title ="Data Types",
+          loc ="center left",
+          bbox_to_anchor =(1, 0, 0.5, 1))
+ 
+  plt.setp(autotexts, size = 8, weight ="bold")
+  ax.set_title("Customizing pie chart")
+ 
+  # show plot
+  plt.show()
+  st.balloons()
+  st.pyplot(fig)
+
+def process_src_IP(packets):
+  srcIP=[]
+  for pkt in packets:
+    if IP in pkt:
+      try:
+        srcIP.append(pkt[IP].src)
+        #print(srcIP)
+      except:
+        pass
+  
+  cnt=Counter()
+  for ip in srcIP:
+    cnt[ip] += 1
+  xData=[]
+  yData=[]
+  for ip, count in cnt.most_common():
+    xData.append(ip)
+    yData.append(count)
+
+  table= PrettyTable(["IP", "Count"])
+  for ip, count in cnt.most_common():
+    table.add_row([ip, count])
+
+  fig = plt.figure(figsize = (10, 5))
+  plt.bar(xData,yData)
+  plt.title("Src IP count")
+  plt.xlabel("Src IPs ")
+  plt.ylabel("Number of times it occuered ")
+  plt.xticks(xData, rotation='vertical')
+  plt.show()
+  
+  result = []
+  st.pyplot(fig)
+
+  # for line in table.splitlines():
+  #     splitdata = line.split("|")
+  #     if len(splitdata) == 1:
+  #         continue  # skip lines with no separators
+  #     linedata = []
+  #     for field in splitdata:
+  #         field = field.strip()
+  #         if field:
+  #             linedata.append(field)
+  #     result.append(linedata)
+
+  #     with open('pcapstats.csv', 'wb') as outcsv:
+  #       writer = csv.writer(outcsv)
+  #       writer.writerows(result)
+
+  #st.markdown(filedownload(writer), unsafe_allow_html=True) 
+
+def main():
+  st.sidebar.title("File Type")
+  file_type = st.sidebar.selectbox('To Upload', ['Dataset', 'Packet capture'], key='1')
+
+  if file_type == "Dataset":
+    st.subheader("Dataset")
+    data_file = st.file_uploader("Upload CSV",type=['csv'])
+    if st.button("Process"):
+      if data_file is not None:
+        file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
+        st.write(file_details)
+        df = pd.read_csv(data_file)
+        st.dataframe(df)
+  elif file_type == "Packet capture":
+    st.subheader("Packet capture")
+    pcap_file = st.file_uploader("Upload PCAP",type=None)
+    if st.button("Process"):
+      if pcap_file is not None:
+        file_details = {"Filename":pcap_file.name,"FileType":pcap_file.type,"FileSize":pcap_file.size}
+        st.write(file_details)
+        packets = rdpcap(pcap_file)
+        process_src_IP(packets)
+        read_pcap(packets)
+        
+  
+  else:
+    print("Incoorect file format")
+
+if __name__=='__main__':
+  main()
